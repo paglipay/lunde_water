a015a462f7f084a3c32684457c4c447700a94fc9
koyeary <katyeary@gmail.com>
2021-02-17 16:19:57 -0800

    add enzyme to Dialogue test

--- client/src/components/auth/register/dialogue/__tests__/Dialogue.test.js
+++ client/src/components/auth/register/dialogue/__tests__/Dialogue.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Dialogue from '../Dialogue';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Dialogue />, div);
+describe('Register dialogue component', () => {
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Dialogue />, div);
+      });
+    
+    
+
 });
+
