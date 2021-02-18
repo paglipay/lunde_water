2ae1096371320c457465898d69b497d44514c167
Paul Aglipay <paglipay@gmail.com>
2021-02-18 15:59:33 -0800

    css fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'semantic-ui-css/semantic.min.css'
+import App from './containers/App';
+import './index.css'
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
