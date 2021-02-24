4a31618916262cdcba80e9103a73d5cbf97e13ab
Paul Aglipay <paglipay@gmail.com>
2021-02-24 14:16:56 -0800

    dynamic forms and orders

--- client/src/components/profile/Data.js
+++ client/src/components/profile/Data.js
@@ -17,22 +17,33 @@ const questions = [
   },
   {
     question: 'Zip Code', type: "text"
-  }  
-  ]
-  
-  export const questions_arry = [
-    { headline: 'Your Profile', questions: questions }
-  ]
-  
-  export const homeObjOne = {
-    topLine: 'New Customer Questions',
-    headline: 'Please Answer Questions',
-    description: 'TEST DESCRIPTION',
-    buttonLabel: 'Next',
-    imgStart: 'start',
-    img: 'images/svg-2.svg',
-    alt: 'Credit Card',
-    onClick: () => console.log('goto Qustion2'),
-    onClickBack: () => console.log('goto Qustion2')
-  };
-  
+  }
+]
+const questions2 = [
+  {
+    question: 'Height', type: "text"
+  },
+  {
+    question: 'Weight', type: "text"
+  },
+  {
+    question: 'Gender', type: "text"
+  },
+]
+
+export const questions_arry = [
+  { headline: 'Your Profile', questions: questions },
+  // { headline: 'Your More Personal Stuff', questions: questions2 }
+]
+
+export const homeObjOne = {
+  topLine: 'New Customer Questions',
+  headline: 'Please Answer Questions',
+  description: 'TEST DESCRIPTION',
+  buttonLabel: 'Next',
+  imgStart: 'start',
+  img: 'images/svg-2.svg',
+  alt: 'Credit Card',
+  onClick: () => console.log('goto Qustion2'),
+  onClickBack: () => console.log('goto Qustion2')
+};
