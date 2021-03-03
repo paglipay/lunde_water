67989c92e6ccc9bfd81028ac0c01ffa1b06b5b2a
Paul Aglipay <paglipay@gmail.com>
2021-03-03 14:38:51 -0800

    merge folders test

--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Layout from '../semantic_ui/ResponsiveLayout/Layout';
 import { homeObjOne, questions_arry } from './Data';
 import { dynamicForm } from '../dynamicform/dynamicform'
-import { restructureForQuestion } from './helperFunctions'
+import { restructureQuestionsForPost } from './helperFunctions'
 
 function Register(props) {
 
@@ -15,7 +15,7 @@ function Register(props) {
       setQuestionIndex(questionIndex + 1)
     }else{
         console.log('Submit', questionsPost)            
-        props.addAnswersToPost(restructureForQuestion(questionsPost))
+        props.addAnswersToPost(restructureQuestionsForPost(questionsPost))
         props.addAnswersRequest(questionsPost)
         props.history.push('/orders')
     }
@@ -25,7 +25,6 @@ function Register(props) {
       setQuestionIndex(questionIndex - 1)
     }else{
       console.log('Submit', questionsPost)   
-
     }
   }
 
